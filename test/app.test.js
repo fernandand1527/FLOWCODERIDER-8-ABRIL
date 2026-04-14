70fd7af26ada5e11ef743a2dd41489facc5723bb
fernandand1527 <fernandand1527@gmail.com>
2026-04-13 20:38:05 -0500

    Se agrega test de resta y correcciones

--- test/app.test.js
+++ test/app.test.js
@@ -1,40 +1,49 @@
-import { saludar, despedir, estadoSistema, Sumar } from "../src/app.js"
+import { saludar, despedir, estadoSistema, Sumar, Restar } from "../src/app.js";
 
 function ejecutarPruebas() {
     let pasadas = 0;
     let fallidas = 0;
 
-    // Test 1
-    const r1 = saludar("Rider")
+    
+    const r1 = saludar("Rider");
     if (r1.includes("Rider")) {
-        console.log("Test 1 Pasado: funcion saludar en CodeRider es correcto")
+        console.log("Test 1 Pasado");
         pasadas++;
     } else {
-        console.log("Test 1 Fallido: ", r1);
+        console.log("Test 1 Fallido:", r1);
         fallidas++;
     }
 
-    // Test 2 (Corregido con IF para que no imprima ambos)
     const estado = estadoSistema(); 
     if (estado === "Activo") {
-        console.log("Test 2 Pasado: ", estado);
+        console.log("Test 2 Pasado");
         pasadas++;
     } else {
-        console.log("Test 2 Fallido: ", estado);
+        console.log("Test 2 Fallido:", estado);
         fallidas++;
     }
 
-    // Test 3 (Corregido: usamos números reales 2 y 3)
-    const Suma = Sumar(2, 3);
-    if (Suma === 5) {
-        console.log("Test 3 Pasado: Sumar(2,3) es correcto");
+    
+    const suma = Sumar(2, 3);
+    if (suma === 5) {
+        console.log("Test 3 Pasado");
         pasadas++;
     } else {
-        console.log("Test 3 Fallido: Sumar(2,3) no es correcto");
+        console.log("Test 3 Fallido");
         fallidas++;
     }
 
-    console.log("\nResultados: " + pasadas + " pasadas, " + fallidas + " fallidas.");
+    
+    const resta = Restar(5, 3);
+    if (resta === 2) {
+        console.log("Test 4 Pasado");
+        pasadas++;
+    } else {
+        console.log("Test 4 Fallido");
+        fallidas++;
+    }
+
+    console.log(`\nResultados: ${pasadas} pasadas, ${fallidas} fallidas.`);
 }
 
 ejecutarPruebas();
